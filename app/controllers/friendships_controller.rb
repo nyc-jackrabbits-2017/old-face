e0ce5c3b52c13e06c7e9b6f9a58da76aa058517f
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-01-06 11:37:25 -0500

    add replace every @user with current_user

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,17 +2,17 @@ class FriendshipsController < ApplicationController
 
   # show a list of friends and pending requests
   def index 
-  	@user = User.find(params[:user_id])
-  	@friends = @user.friends 
-  	@pending_incoming = @user.pending_incoming_requests
-  	@pending_outgoing =	@user.pending_outgoing_requests
+  	
+  	@friends = current_user.friends 
+  	@pending_incoming = current_user.pending_incoming_requests
+  	@pending_outgoing =	current_user.pending_outgoing_requests
   end 
 
 
   def update
     
-    @user = User.find(params[:user_id])
-  	@friendship = @user.friendships_1.find_by(sender_id: params[:id])
+    
+  	@friendship = current_user.friendships_1.find_by(sender_id: params[:id])
   	# binding.pry
   	@friendship.update_attributes(accepted: true)
  
@@ -23,8 +23,8 @@ class FriendshipsController < ApplicationController
   end 
 
   def destroy
-  	@user = User.find(params[:user_id])
-  	@friendship = @user.friendships_1.find_by(sender_id: params[:id])
+  	
+  	@friendship = current_user.friendships_1.find_by(sender_id: params[:id])
   	@friendship.destroy
   	redirect_to root_url
   	#redirect to the index page
